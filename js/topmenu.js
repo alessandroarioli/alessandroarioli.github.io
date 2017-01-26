cc539ba764cf15dc21255e3ca91c1a1b3c2da1f9
AleArioli <alessandro.arioli@italianway.it>
2017-01-26 23:21:08 +0100

    restyling entire portfolio :)

--- js/topmenu.js
+++
@@ -1,9 +0,0 @@
-var num = 600;
-
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() > num) {
-        $('#myNavbar').addClass('fixed');
-    } else {
-        $('#myNavbar').removeClass('fixed');
-    }
-});
